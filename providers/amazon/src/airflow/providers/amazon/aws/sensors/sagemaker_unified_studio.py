2a1ce455d5a0fd2808775cb6d2e088654982d3c5
kgw7401 <kgw7401@gmail.com>
2025-07-01 08:25:22 +0900

    add: version_compat

--- providers/amazon/src/airflow/providers/amazon/aws/sensors/sagemaker_unified_studio.py
+++ providers/amazon/src/airflow/providers/amazon/aws/sensors/sagemaker_unified_studio.py
@@ -30,7 +30,7 @@ from airflow.providers.amazon.version_compat import AIRFLOW_V_3_0_PLUS
 if AIRFLOW_V_3_0_PLUS:
     from airflow.sdk import BaseSensorOperator
 else:
-    from airflow.sensors.base import BaseSensorOperator  # type: ignore[no-redef]
+    from airflow.providers.amazon.version_compat import BaseSensorOperator
 
 if TYPE_CHECKING:
     from airflow.utils.context import Context
